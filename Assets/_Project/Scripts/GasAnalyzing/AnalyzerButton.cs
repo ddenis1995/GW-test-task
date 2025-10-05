24cae4a95debb3c7d7ef76f4e23cf820be0eb5a8
ddenis1995 <davylyn@gmail.com>
2025-10-06 01:09:02 +0400

    New scene

---
+++ Assets/_Project/Scripts/GasAnalyzing/AnalyzerButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HTC.UnityPlugin.ColliderEvent;
+using UnityEngine;
+
+
+public class AnalyzerButton : MonoBehaviour
+    , IColliderEventPressEnterHandler
+    , IColliderEventPressExitHandler
+
+{
+    public void OnColliderEventPressEnter(ColliderButtonEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnColliderEventPressExit(ColliderButtonEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+}
